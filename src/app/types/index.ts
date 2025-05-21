ed767528aeaf9767577f8183ab5500ab581ce81a
thecodingmontana <dev.humblechris254@gmail.com>
2025-05-21 17:28:39 +0300

    chore(app): add banner for tv and movie info

--- src/app/types/index.ts
+++ src/app/types/index.ts
@@ -16,3 +16,118 @@ export interface Poster {
   vote_count: number;
   name: string;
 }
+
+export type BaseMedia = {
+  adult: boolean;
+  backdrop_path: string | null;
+  genres: { id: number; name: string }[];
+  homepage: string;
+  id: number;
+  origin_country: string[];
+  original_language: string;
+  overview: string;
+  popularity: number;
+  poster_path: string | null;
+  production_companies: {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  spoken_languages: {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+  }[];
+  status: string;
+  tagline: string;
+  vote_average: number;
+  vote_count: number;
+};
+
+export type TVMedia = BaseMedia & {
+  media_type: 'tv';
+  name: string;
+  created_by: {
+    id: number;
+    credit_id: string;
+    name: string;
+    original_name: string;
+    gender: number;
+    profile_path: string | null;
+  }[];
+  episode_run_time: number[];
+  first_air_date: string;
+  in_production: boolean;
+  languages: string[];
+  last_air_date: string;
+  last_episode_to_air: {
+    id: number;
+    name: string;
+    overview: string;
+    vote_average: number;
+    vote_count: number;
+    air_date: string;
+    episode_number: number;
+    episode_type: string;
+    production_code: string;
+    runtime: number;
+    season_number: number;
+    show_id: number;
+    still_path: string | null;
+  };
+  next_episode_to_air: {
+    id: number;
+    name: string;
+    overview: string;
+    vote_average: number;
+    vote_count: number;
+    air_date: string;
+    episode_number: number;
+    episode_type: string;
+    production_code: string;
+    runtime: number;
+    season_number: number;
+    show_id: number;
+    still_path: string | null;
+  };
+  networks: {
+    id: number;
+    logo_path: string;
+    name: string;
+    origin_country: string;
+  }[];
+  number_of_episodes: number;
+  number_of_seasons: number;
+  original_name: string;
+  seasons: {
+    air_date: string;
+    episode_count: number;
+    id: number;
+    name: string;
+    overview: string;
+    poster_path: string | null;
+    season_number: number;
+    vote_average: number;
+  }[];
+  type: string;
+};
+
+export type MovieMedia = BaseMedia & {
+  media_type: 'movie';
+  title: string
+  belongs_to_collection: any;
+  budget: number;
+  imdb_id: string;
+  original_title: string;
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  video: boolean;
+};
+
+export type Media = TVMedia | MovieMedia;
